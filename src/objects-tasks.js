c4b9188b6f0c0138a7fbb9a4708597d3e4c828e0
Dmitry Kuchukov <kudim@inbox.ru>
2024-02-28 14:49:36 +0300

    feat: rady task 3

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -63,8 +63,13 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const result = obj;
+  keys.forEach((item) => {
+    delete result[item];
+  });
+
+  return result;
 }
 
 /**
